6b3123ff906ec759ed29dd1588b708eb8ca8579c
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-03-22 02:08:35 +0100

    refactoring

--- deno/main.ts
+++ deno/main.ts
@@ -1,14 +1,11 @@
 import { NodeBridge } from "../adapter/deno.ts";
 
-const node = await NodeBridge.connect<typeof import("../node/main.ts").nodeExports>(new URL('../node/main.ts', import.meta.url));
-
-node.helloCallOnNode(3)
-
 export const denoExports = NodeBridge.export({
 	hello(a:number) {
 		return 1
 	}
 })
 
-
+const node = await NodeBridge.connect<typeof import("../node/main.ts").nodeExports>(new URL('../node/main.ts', import.meta.url));
+node.helloCallOnNode(3)
 console.log("node",node)
